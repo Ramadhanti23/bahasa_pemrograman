28c30492aa2f21bfde6b35656350da4a6df725fa
Ramadhanti23 <elvinaramadhanti@gmail.com>
2022-12-13 14:45:54 +0700

    update

--- bahasa_pemograman/sesi10grafik.py
+++ bahasa_pemograman/sesi10grafik.py
@@ -15,11 +15,11 @@
 # plt.show()
 
 
-# import matplotlib.pyplot as plt
-# fig = plt.figure()
-# ax = fig.add_axes([0,0,1,1])
-# ax.axis('equal')
-# MataKuliah = ['Matematika', 'Fisika', 'Kimia', 'Komputer', 'Bahasa']
-# Nilai = [80,90,65,79,82]
-# ax.pie(Nilai,labels=MataKuliah,autopct='%1.2f%%')
-# plt.show()
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.axis('equal')
+MataKuliah = ['Matematika', 'Fisika', 'Kimia', 'Komputer', 'Bahasa']
+Nilai = [80,90,65,79,82]
+ax.pie(Nilai,labels=MataKuliah,autopct='%1.2f%%')
+plt.show()
