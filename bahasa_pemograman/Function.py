2c47554ce5ac13a5b2500812feed5c5b8a77ebca
Ramadhanti23 <elvinaramadhanti@gmail.com>
2022-12-19 22:58:25 +0700

    Function.py

---
+++ bahasa_pemograman/Function.py
@@ -0,0 +1,19 @@
+#Contoh Function 
+
+def function():
+    print(" Tugas Function Di Python ")
+    
+#Function with Arguments  
+
+def function_with_args(fname):
+    print(fname + "20210801204")
+
+def function_2_args(fname, lname):
+    print(fname + " " + lname)
+
+
+function()
+
+function_with_args(" Ramadhanti Elvina P : ")
+
+function_2_args(" Sesi ", " 11 ")
